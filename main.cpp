ef8169effd0b43ae3264220754656a5ea0242538
Rahul Goel <rahulgoellko@gmail.com>
2022-11-24 11:34:51 +0530

    improved skeleton

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <queue>
 #include <functional>
 
+#define DEBUG
+
 const int TICK_RATE = 1;
 
 class Task {
@@ -20,6 +22,7 @@ class Job : public Task {
     
     // start time
     int start;
+    int exec_time = 0;
 };
 
 // comparator for EDF
@@ -31,6 +34,8 @@ int main() {
     // list of tasks
     std::vector<Task> periodic_tasks;
     for (int i = 0; i < 10; ++i) {
+        Task t(rand() % 10, rand() % 10, rand() % 10, rand() % 10);
+        periodic_tasks.push_back(t);
     }
 
     // initial jobs
@@ -41,17 +46,35 @@ int main() {
     }
 
     // priority queue
-    std::priority_queue<Job, std::vector<Job>, std::function<bool(Job, Job)>> pq(jobs.begin(), jobs.end());
+    std::priority_queue<Job, std::vector<Job>, std::function<bool(Job, Job)>> pq(jobs.begin(), jobs.end(), cmp_edf);
 
     // run simulation
     for (int tiktok = 0; ; ++tiktok) {
         // does any job need to be added at this instant?
+        for (Task &task : periodic_tasks) {
+            if (tiktok >= task.phi and (tiktok - task.phi) % task.p == 0) {
+                pq.push(Job(task.phi, task.p, task.e, task.d, tiktok));
+                #ifdef DEBUG
+                std::cout << tiktok << " " << "Added task to queue." << std::endl;
+                #endif
+            }
+        }
 
         // choose which job to execute based on the priority
+        Job job = pq.top();
+        pq.pop();
 
         // execute the job
+        job.exec_time += 1;
 
-        // remove the jobs that have finished their execution
+        // if the chosen job is not done with it's execution, then put it back in the queue
+        if (job.exec_time != job.e) {
+            pq.push(job);
+        }
+
+        #ifdef DEBUG
+        std::cout << tiktok << " The current queue size is " << pq.size() << std::endl;
+        #endif
     }
 
     return 0;
